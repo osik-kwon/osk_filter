7bf10fae056eb52cf840decbc1c44be96813d6ac
osik-kwon <zzzharuki2@gmail.com>
2019-08-28 10:11:37 +0900

    cryptopp

--- filter/main.cpp
+++ filter/main.cpp
@@ -4,6 +4,22 @@
 #include "hwp/hwp30_syntax.h"
 #include "locale/charset_encoder.h"
 
+#include "cryptopp/cryptlib.h"
+#include "cryptopp/Base64.h"
+#include "cryptopp/aes.h"        
+#include "cryptopp/seed.h"
+#include "cryptopp/des.h"
+#include "cryptopp/modes.h"      
+#include "cryptopp/filters.h"  
+
+#ifdef _WIN32
+#ifdef _DEBUG
+#  pragma comment ( lib, "cryptlib_mdd" )
+#else
+#  pragma comment ( lib, "cryptlib_md" )
+#endif
+#endif
+
 void print(const filter::hwp50::filter_t::sections_t& sections)
 {
 	setlocale(LC_ALL, "korean");
@@ -95,11 +111,129 @@ void test_hwp30()
 	//print(filter.extract_all_texts(to_utf8(u"d:/filter/hwp30/hwp97_1.hwp")));
 }
 
+/*
 int main()
 {
 	//test_decompress_save();
 	//test_extract_all_texts();
 	//test_replace_privacy();
 	test_hwp30();
+	return 0;
+}*/
+
+template <class TyMode>
+std::string Encrypt(TyMode& Encryptor, const std::string& PlainText)
+{
+	std::string EncodedText;
+
+	try {
+		CryptoPP::StringSource(PlainText, true,
+			new CryptoPP::StreamTransformationFilter(Encryptor,
+				new CryptoPP::Base64Encoder(
+					new CryptoPP::StringSink(EncodedText), false
+				), CryptoPP::BlockPaddingSchemeDef::ZEROS_PADDING
+			)
+		);
+	}
+	catch (...) {}
+
+	return EncodedText;
+}
+
+template <class TyMode>
+std::string Decrypt(TyMode& Decryptor, const std::string& EncodedText)
+{
+	std::string RecoveredText;
+
+	try {
+		CryptoPP::StringSource(EncodedText, true,
+			new CryptoPP::Base64Decoder(
+				new CryptoPP::StreamTransformationFilter(Decryptor,
+					new CryptoPP::StringSink(RecoveredText),
+					CryptoPP::BlockPaddingSchemeDef::ZEROS_PADDING
+				)
+			)
+		);
+	}
+	catch (...) {}
+
+	return RecoveredText;
+}
+
+typedef uint8_t byte;
+
+template <class Ty>
+std::string CBC_Encrypt(byte* KEY, byte* IV, const std::string& PlainText)
+{
+	typename CryptoPP::CBC_Mode<Ty>::Encryption Encryptor(KEY, Ty::DEFAULT_KEYLENGTH, IV);
+	return Encrypt(Encryptor, PlainText);
+}
+
+
+template <class Ty>
+std::string CBC_Decrypt(byte* KEY, byte* IV, const std::string& PlainText)
+{
+	typename CryptoPP::CBC_Mode<Ty>::Decryption Decryptor(KEY, Ty::DEFAULT_KEYLENGTH, IV);
+	return Decrypt(Decryptor, PlainText);
+}
+
+template <class Ty>
+std::string ECB_Encrypt(byte* KEY, const std::string& PlainText)
+{
+	typename CryptoPP::ECB_Mode<Ty>::Encryption Encryptor(KEY, Ty::DEFAULT_KEYLENGTH);
+	return Encrypt(Encryptor, PlainText);
+}
+
+
+template <class Ty>
+std::string ECB_Decrypt(byte* KEY, const std::string& PlainText)
+{
+	typename CryptoPP::ECB_Mode<Ty>::Decryption Decryptor(KEY, Ty::DEFAULT_KEYLENGTH);
+	return Decrypt(Decryptor, PlainText);
+}
+
+
+template <class CryptoType>
+void Test()
+{
+	using namespace std;
+
+	const std::string sText = "Plain Text";
+	std::string sEnc, sDec;
+
+	byte KEY[CryptoType::DEFAULT_KEYLENGTH] = { 0, };
+	byte IV[CryptoType::BLOCKSIZE] = { 0x01, };
+
+	// CBC 모드
+	sEnc = CBC_Encrypt<CryptoType>(KEY, IV, sText);
+	sDec = CBC_Decrypt<CryptoType>(KEY, IV, sEnc);
+
+	cout << CryptoType::StaticAlgorithmName() << " : " << "CBC_MODE" << endl;
+	cout << sText << "\n -> " << sEnc << "\n -> " << sDec << endl;
+
+
+	// ECB 모드
+	sEnc = ECB_Encrypt<CryptoType>(KEY, sText);
+	sDec = ECB_Decrypt<CryptoType>(KEY, sEnc);
+
+	cout << CryptoType::StaticAlgorithmName() << " : " << "ECB_MODE" << endl;
+	cout << sText << "\n -> " << sEnc << "\n -> " << sDec << endl;
+	cout << endl;
+}
+
+
+int main()
+{
+	using namespace std;
+
+	// SEED 
+	Test<CryptoPP::SEED>();
+
+	// AES 
+	Test<CryptoPP::AES>();
+
+	// DES 
+	Test<CryptoPP::DES>();
+
 	return 0;
 }
